90c8ab76855c63d203287ed9ba14beef3d098e52
ZabaluevDenis <buket24.by@gmail.com>
2022-05-28 19:39:40 +0300

    Создание объектов JavaScript

---
+++ lesson 83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Great Spirit",
+    legs: 4,
+    tails: 1,
+    friends: ["another dogs", "human"]
+
+    // Only change code above this line
+};
