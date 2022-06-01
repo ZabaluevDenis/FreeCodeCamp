61130f6902d64cd581011706a48686ab72492f86
Денис <105744153+ZabaluevDenis@users.noreply.github.com>
2022-06-01 14:31:55 +0300

    Создание случайных дробей с помощью JavaScript

---
+++ lesson 105.js
@@ -0,0 +1,9 @@
+function randomFraction() {
+    return Math.random();
+    // Only change code below this line
+
+    return 0;
+
+    // Only change code above this line
+}
+console.log(randomFraction());
